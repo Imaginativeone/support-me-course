aa8f9b7771c8f3efd261940db9bb47d0daadd719
Doug Franklin <doug@dougfranklin.com>
2024-06-30 10:02:57 -0400

    ShadCN buttons work

---
+++ app/(logged-out)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <h1>SupportMe</h1>
+      <Button>Log In</Button>
+      <Button>Sign Up</Button>
+    </div>
+  )
+}
