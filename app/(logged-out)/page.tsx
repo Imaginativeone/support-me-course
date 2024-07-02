ff42c7b3eef6f5de2624cf2bea96bfc6e7716576
Doug Franklin <dfranklin@copt.com>
2024-07-02 14:27:14 -0400

    Added the PersonStanding icon

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { PersonStandingIcon } from "lucide-react";
 
 export default function LandingPage() {
   return (
     <>
-      <h1>SupportMe</h1>
+      <h1 className="flex gap-2">
+        <PersonStandingIcon />SupportMe
+      </h1>
       <Button>Log In</Button>
       <Button>Sign Up</Button>
     </>
