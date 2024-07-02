da4f44c6097f952c895975f7cb44721a8ffbc67c
Doug Franklin <dfranklin@copt.com>
2024-07-02 15:35:35 -0400

    End of Video 06

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
 import "./globals.css";
 
-import { cn } from "@/lib/utils";
-
-const poppins = Poppins ({ subsets: ["latin"], weight: [
-                                                "100", "200", "300", "400", "500", "600", "700", "800", "900"
-                                              ] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,11 +15,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
+    <html lang='en' data-theme='dark'>
+    {/* <html lang="en"> */}
       <body className={cn(poppins.className, "dark")}>{children}</body>
     </html>
   );
