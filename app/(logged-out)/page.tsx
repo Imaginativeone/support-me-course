75e43c38bd13699db411c71033150f568113bced
Doug Franklin <dfranklin@copt.com>
2024-07-02 14:33:33 -0400

    Tagline and Buttons aligned together

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -4,11 +4,14 @@ import { PersonStandingIcon } from "lucide-react";
 export default function LandingPage() {
   return (
     <>
-      <h1 className="flex gap-2">
-        <PersonStandingIcon />SupportMe
+      <h1 className="flex gap-2 items-center">
+        <PersonStandingIcon size={50} className="text-pink-500" />SupportMe
       </h1>
-      <Button>Log In</Button>
-      <Button>Sign Up</Button>
+      <p>The best dashboard to manage customer support</p>
+      <div>
+        <Button>Log In</Button>
+        <Button>Sign Up</Button>
+      </div>
     </>
   )
 }
