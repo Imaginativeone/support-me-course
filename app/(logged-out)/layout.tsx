4ba13170d49e31c78db40adf0b68835d62c4d93e
Doug Franklin <dfranklin@copt.com>
2024-07-03 09:59:09 -0400

    Dark Mode Toggle Works (with tooltip)

--- app/(logged-out)/layout.tsx
+++ app/(logged-out)/layout.tsx
@@ -1,5 +1,7 @@
 // import { LightDarkToggle } from "@/components/ui/light-dark-toggle";
 
+import { LightDarkToggle } from "@/components/ui/light-dark-toggle";
+
 type Props = {
   children?: React.ReactNode;
 };
@@ -11,6 +13,7 @@ export default function LoggedOutLayout({ children }: Props) {
         {children}
       </div>
       {/* <LightDarkToggle className="fixed top-[calc(50%-12px)] right-2" /> */}
+      <LightDarkToggle />
     </>
   );
 }
--- components/ui/light-dark-toggle.tsx
+++ components/ui/light-dark-toggle.tsx
@@ -1,9 +1,26 @@
 'use client'
 
 import { useState } from "react"
+import { Tooltip, TooltipProvider } from "./tooltip"
+import { TooltipContent, TooltipTrigger } from "@radix-ui/react-tooltip"
+import { MoonIcon, SunIcon } from "lucide-react"
 
 export function LightDarkToggle() {
   
   const [isDarkMode, setIsDarkMode] = useState(true)
   
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger onClick={() => {
+          setIsDarkMode(prevValue => !prevValue)
+        }}>
+          { isDarkMode ? <MoonIcon /> : <SunIcon /> }
+        </TooltipTrigger>
+        <TooltipContent>
+          { isDarkMode ? "Enable Light Mode" : "Enable Dark Mode" }
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
 }
