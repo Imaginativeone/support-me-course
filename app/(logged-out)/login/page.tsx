096dddd2fdff370eed92a7035b9a151c95e9b36e
Doug Franklin <dfranklin@copt.com>
2024-07-04 07:43:26 -0400

    Video-10 complete

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -1,33 +1,105 @@
-'use client'
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { PasswordInput } from "@/components/ui/password-input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { PersonStandingIcon } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { PersonStandingIcon } from "lucide-react"
-import Link from "next/link"
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
 
 export default function LoginPage() {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log("login validation passed");
+    router.push("/dashboard");
+  };
+
   return (
     <>
       <PersonStandingIcon size={50} />
       <Card className="w-full max-w-sm">
         <CardHeader>
-          <CardTitle>
-            Login
-          </CardTitle>
-          <CardDescription>
-            Login to Your SupportMe Account
-          </CardDescription>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Login to your SupportMe account</CardDescription>
         </CardHeader>
         <CardContent>
-          login form
+          <Form {...form}>
+            <form
+              className="flex flex-col gap-4"
+              onSubmit={form.handleSubmit(handleSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="john@doe.com" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is the email address you signed up to SupportMe with
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <PasswordInput placeholder="••••••••" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Login</Button>
+            </form>
+          </Form>
         </CardContent>
         <CardFooter className="justify-between">
-          Don't have an account?
+          <small>Don't have an account?</small>
           <Button asChild variant="outline" size="sm">
-            <Link href="/sign-up">Sign Up</Link>
+            <Link href="/sign-up">Sign up</Link>
           </Button>
         </CardFooter>
       </Card>
     </>
-  )
+  );
 }
